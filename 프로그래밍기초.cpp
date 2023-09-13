a43390ad5c97d1a891a9446d1ea272a00fd15d54
Heewon <Heewon@DESKTOP-SS4O1AQ>
2023-09-13 23:53:01 +0900

    초기 커밋.

---
+++ 프로그래밍기초.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int sum(int* a, int* b)
+{
+    *a += 30;
+    return *a + *b;
+}
+
+int main(int argc, char const* argv[])
+{
+    int a = 10, b = 20;
+    cout << sum(&a, &b) << endl;
+    cout << a << endl;
+
+    return 0;
+}
